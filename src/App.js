a0af0d141fc48212f236b57d8f0f495350709d15
Adrian Terrazas <darktega@gmail.com>
2021-06-27 23:53:38 -0500

    Basic page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Metronome</h1>
+      <p>BPM: {}</p>
+      <input 
+        type="range"
+        min="40"
+        max="280"
+        step="1"
+        onChange={() => { alert(`Works`); }}
+      />
     </div>
   );
 }
